adcb4e6927364edb5865e6b36d2b327f064bd066
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-02 01:44:46 +0300

    commit tree 7e3540a0eb27a5ccfeef5a9471798e34e1399f2c

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+  return a + b
+}
+add(2,2)
+function subtract(a,b){
+  return a - b
+}
+subtract(3,3)
+function multiply(a,b){
+  return a * b
+}
+multiply(2,3)
+function divide(a,b){
+  return a / b
+}
+divide(5 ,10)
+
+function increment(n) {
+    return n+=1
+}
+increment(10)
+
+function decrement(n){
+    return n-=1
+}
+decrement(10)
+
+ function makeInt(n) {
+    return parseInt(n,10)
+ }
+ makeInt(10)
+ 
+ function preserveDecimal(n) {
+     return n % 4
+ }
+ preserveDecimal(19)
